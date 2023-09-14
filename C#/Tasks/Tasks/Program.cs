c0db57576eedfa6d5c78e779907cf250a2641a8c
Stas_H1p <bbelaci@mail.ru>
2023-09-14 12:32:16 +0300

    The second task is done.

--- C#/Tasks/Tasks/Program.cs
+++ C#/Tasks/Tasks/Program.cs
@@ -1,38 +1,83 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿//Sarcina 1
+//a) citește de la tastatura un număr cu minim de 3 cifre dacă
+//numărul are mai puțin de 3 cifre, îi spune utilizatorului
+//acest lucru și îi cere alt număr
+//b) dacă numărul este corect, calculeaza valoarea in
+//oglinda (ex 441 in oglinda este 144)
+//c) verifica și îi spune userului dacă numărul dat în oglinda
+//este pătrat perfect (ex 144 = 12 * 12 - ok)
+
 using System;
-Console.WriteLine("Enter number:");
-int number = int.Parse(Console.ReadLine());
-string numStr = number.ToString();
+using System.ComponentModel.Design;
 
-while (numStr.Length < 3)
+Console.WriteLine("Enter number: ");
+string input = Console.ReadLine();
+string[] cuvinte = input.Split(' ');
+double[] numere = new double[cuvinte.Length];
+for (int i = 0; i < cuvinte.Length; i++)
 {
-    Console.WriteLine("You number is wrong:");
+    numere[i] = Convert.ToDouble(cuvinte[i]);
+}
+Console.WriteLine("The numbers are: ");
+foreach (var numar in numere)
+{
+    Console.WriteLine(numar);
+}
+
+foreach (var numar in numere)
+{
+    if (numar % 1 == 0)
+    {
+        Console.WriteLine("Integer: " + numar);
+    }
+    else
+    {
+        Console.WriteLine("Not integer: " + numar);
+    }
+}
+double minNumber = (double)numere[0];
+foreach (var numar in numere)
+{
+    if (minNumber > numar)
+    {
+        minNumber = numar;
+    }
+}
+Console.WriteLine("Min number is: " + minNumber);
+void Task1()
+{
     Console.WriteLine("Enter number:");
-    number = int.Parse(Console.ReadLine());
-    numStr = number.ToString();
-}
+    int number = int.Parse(Console.ReadLine());
+    string numStr = number.ToString();
 
-int oglindaNumber = ReverseNumber(numStr);
-Console.WriteLine("The number is: " + numStr);
-Console.WriteLine("Reversed number is : " + oglindaNumber);
+    while (numStr.Length < 3)
+    {
+        Console.WriteLine("You number is wrong:");
+        Console.WriteLine("Enter number:");
+        number = int.Parse(Console.ReadLine());
+        numStr = number.ToString();
+    }
 
-double result = Math.Sqrt(oglindaNumber);
-if (result % 1 == 0)
-{
-    Console.WriteLine("Is perfect square:");
-}
-else
-{
-    Console.WriteLine("Is not perfect square:");
-}
+    int oglindaNumber = ReverseNumber(numStr);
+    Console.WriteLine("The number is: " + numStr);
+    Console.WriteLine("Reversed number is : " + oglindaNumber);
 
-int ReverseNumber(string numStr)
-{
-    char[] charArray = numStr.ToCharArray();
-    Array.Reverse(charArray);
-    string reversedStr = new string(charArray);
-    int reversedNum = int.Parse(reversedStr);
-    return reversedNum;
-}
+    double result = Math.Sqrt(oglindaNumber);
+    if (result % 1 == 0)
+    {
+        Console.WriteLine("Is perfect square:");
+    }
+    else
+    {
+        Console.WriteLine("Is not perfect square:");
+    }
 
-//Console.WriteLine("Your number is "number);
+    int ReverseNumber(string numStr)
+    {
+        char[] charArray = numStr.ToCharArray();
+        Array.Reverse(charArray);
+        string reversedStr = new string(charArray);
+        int reversedNum = int.Parse(reversedStr);
+        return reversedNum;
+    }
+}
