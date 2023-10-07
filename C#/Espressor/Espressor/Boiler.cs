364bdebc436292dc45526509a11a813b0e98beaf
Stas_H1p <bbelaci@mail.ru>
2023-10-07 20:33:32 +0300

    New task

---
+++ C#/Espressor/Espressor/Boiler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Espressor
+{
+    public  class Boiler
+    {
+        public float watherLevel; //maximum 3 l
+        private Indicator indicator = new Indicator();
+
+        public Boiler()
+        {
+            float min = 0f;
+            float max = 3000f;
+            Random random = new Random();
+            float watherLevel = (float)(min + (random.NextDouble() * (max - min)));
+        }
+        public bool checkLevelWather()
+        {
+            if (watherLevel > 0.5)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        public void AddWather(float watherML)
+        {
+            if (watherML + watherLevel <= 3)
+                watherLevel += watherML;
+            else
+            {
+                Console.WriteLine("E prea multa apa.");
+                indicator.color = "RED";
+            }
+        }
+    }
+}
---
+++ C#/Espressor/Espressor/Espressor.cs
@@ -0,0 +1,52 @@
+﻿using System;
+namespace Espressor
+{
+    public class Expressor
+    {
+
+        protected Boiler boil = new Boiler();
+        public void Menu()
+        {
+            Console.WriteLine("===========Espressor===========");
+            Console.WriteLine("1. Start");
+            Console.WriteLine("2. Verificarea apei");
+            Console.WriteLine("3. Pune suportul pentru apa");
+            Console.WriteLine("0. Exit.=(");
+            int nrMenu = Convert.ToInt32(Console.ReadLine());
+
+            if (nrMenu >= 0 && nrMenu <= 10)
+            {
+                switch (nrMenu)
+                {
+                    case 1:
+                        Console.WriteLine("Este elictricitate.");
+                        break;
+                    case 2:
+                        if (boil.checkLevelWather())
+                        {
+                            Console.WriteLine($"Nivelul apei este {boil.watherLevel}");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Nivelul apei nu este suficient {boil.watherLevel}");
+
+                        }
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Opss optiune incorecta. :(");
+            }
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Expressor masina = new Expressor();
+            masina.Menu();
+        }
+    }
+}
